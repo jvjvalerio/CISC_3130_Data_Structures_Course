f44b7112959efa6856277ea151e655397f44246b
Julio Valerio <Julio.Valerio@csi.cuny.edu>
2022-02-03 17:38:30 -0500

    Update Assignment_1 file as per professors request Add Master_File

--- Assignment_1.java
+++ Assignment_1.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Assignment_1 {
 
-    // Method to read our transaction file
-    public static ArrayList<String> readData() throws Exception {
+    // Method to read our Transaction file
+    public static ArrayList<String> readTransactionFile() throws Exception {
         ArrayList<String> transactions = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader("Transactions.txt"));
         String line = null;
@@ -17,61 +16,69 @@ public class Assignment_1 {
         return transactions;
     }
 
-    // Method for creating separate Array for the "O's"
-    public static ArrayList<String> processOrders (ArrayList<String>  arr) {
-        ArrayList<String> orderTransactions = new ArrayList<String>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i).charAt(0) == 'O') {
-                orderTransactions.add(arr.get(i)); 
+    // Method to read our Master file
+    public static ArrayList<String> readMasterFile() throws Exception {
+        ArrayList<String> master = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader("Master_File.txt"));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            master.add(line);
+        }
+        reader.close();
+        return master;
+    }
+
+    // Method for processing Transactions
+    public static ArrayList<String> processTransactions(ArrayList<String> allTransactions, ArrayList<String> allMaster) {
+        String[] placeHolder = new String[allTransactions.size()];
+        String[] allMasterPlaceHolder = new String[allMaster.size()];
+        for (int i = 0; i < allTransactions.size(); i++) {
+            placeHolder = allTransactions.get(i).split("\\s+");
+            if (placeHolder[0].equals("O")) {
+                int index = findCustomerID(placeHolder[1], allMaster);
+                allMasterPlaceHolder = allMaster.get(index).split("\\s+");
+                float discountPrice = Float.parseFloat(placeHolder[5]) * Float.parseFloat(placeHolder[6]) / 100;
+                float cost = discountPrice * Float.parseFloat(placeHolder[4]);
+                float balance = Float.parseFloat(allMasterPlaceHolder[2]);
+                balance += cost;
+                allMasterPlaceHolder[2] = Float.toString(balance);
+                String newRecord = String.join(",", allMasterPlaceHolder);
+                allMaster.set(index, newRecord);
+            } else if (placeHolder[0].equals("P")) {
+                int index = findCustomerID(placeHolder[1], allMaster);
+                allMasterPlaceHolder = allMaster.get(index).split("\\s+");
+                float discountPayment = Float.parseFloat(placeHolder[3]) * Float.parseFloat(placeHolder[4]) / 100;
+                float reduceBalance = Float.parseFloat(placeHolder[3]) - discountPayment;
+                float balance = Float.parseFloat(allMasterPlaceHolder[3]);
+                balance -= reduceBalance;
+                allMasterPlaceHolder[2] = Float.toString(balance);
+                String newRecord = String.join(",", allMasterPlaceHolder);
+                allMaster.set(index, newRecord);
             }
         }
-        return orderTransactions;
-    } 
+        return allMaster;
+    }
 
-    // Method for creating separate Array for the "P's"
-    public static ArrayList<String> processPayments (ArrayList<String>  arr) {
-        ArrayList<String> paymentTransactions = new ArrayList<String>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i).charAt(0) == 'P') {
-                paymentTransactions.add(arr.get(i)); 
+    public static int findCustomerID(String customerID, ArrayList<String> allMasterArrayList) {
+        int index = 0;
+        for (int i = 0; i < allMasterArrayList.size(); i++) {
+            if (customerID.equals(allMasterArrayList.get(0))) {
+                index = i;
+                return index;
             }
         }
-        return paymentTransactions;
-    } 
-
-    // Method for processing accounts receivable
-    public static ArrayList<String> accountsReceivable(ArrayList<String> orders, ArrayList<String> payments) {
-        ArrayList<String> masterRecord = new ArrayList<String>();
-        // Customer ID 2000 = julio
-        float juliosBalance = 0;
-        // Customer ID 2001 = brenda
-        float brendasBalance = 0;
-
-        for (int i = 0; i < orders.size(); i++) {
-            String text = orders.get(i);
-            String[] line = text.split("     ");
-            if (line[1].equals("2000")) {
-                juliosBalance +=  (Float.parseFloat(line[4]) * Float.parseFloat(line[5])) - (Float.parseFloat(line[6])/ 100);
-                System.out.println(juliosBalance);
-            System.out.println(Arrays.toString(line));
-            }
-            if (line[1].equals("2001")) {
-                brendasBalance += (Float.parseFloat(line[4]) * Float.parseFloat(line[5])) - (Float.parseFloat(line[6])/ 100);
-            }
-            
-        }
-        return null;
+        return 0;
     }
     
     // Driver Code
     public static void main(String[] args) throws Exception {
         ArrayList<String> allTransactions = new ArrayList<String>();
-        ArrayList<String> orderTransactions = new ArrayList<String>();
-        ArrayList<String> paymentTransactions = new ArrayList<String>();
-        
-        allTransactions = readData();
-        orderTransactions = processOrders(allTransactions);
-        paymentTransactions = processPayments(allTransactions);
-        accountsReceivable(orderTransactions, paymentTransactions);
+        ArrayList<String> allMaster = new ArrayList<String>();
+        ArrayList<String> updatedMaster = new ArrayList<String>();
+
+        allTransactions = readTransactionFile();
+        allMaster = readMasterFile();
+        updatedMaster = processTransactions(allTransactions, allMaster);
+        System.out.println(updatedMaster.toString());
     }
 }
