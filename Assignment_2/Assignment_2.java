6473b842cc4638494f113420a5939055a108c769
Julio Valerio <Julio.Valerio@csi.cuny.edu>
2022-02-23 13:57:49 -0500

    Update Assignment_2.java file with approach to solution and created two files Payroll_Data_File.txt and Personnel_Data_File.txt

--- Assignment_2/Assignment_2.java
+++ Assignment_2/Assignment_2.java
@@ -1,1 +1,79 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+// PayRoll class
+class PayRoll {
+    String employeeNo = "";
+    String numOfDays = "";
+    String startTime = "";
+    String endTime = "";
+
+    // PayRoll Constructor
+    public PayRoll(String employeeNo, String numOfDays, String startTime, String endTime) {
+        this.employeeNo = employeeNo;
+        this.numOfDays = numOfDays;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
+
+// Personnel class
+class Personnel {
+    String employeeNo = "";
+    String lastNameFirstName = "";
+    String streetAddress = "";
+    String cityStateZip = "";
+    String hourlyRate = "";
+
+    // Personnel Constructor
+    public Personnel(String employeeNo, String lastNameFirstName, String streetAddress, String cityStateZip, String hourlyRate) {
+        this.employeeNo = employeeNo;
+        this.lastNameFirstName = lastNameFirstName;
+        this.streetAddress = streetAddress;
+        this.cityStateZip = cityStateZip;
+        this.hourlyRate = hourlyRate;
+    }
+}
+
+public class Assignment_2 {
+    // Method for reading in our Payroll_Data_File
+    public static ArrayList<String> readPayDataFile() throws Exception {
+        ArrayList<String> paymentFile = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader("Payroll_Data_File.txt"));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            paymentFile.add(line);
+        }
+        reader.close();
+        return paymentFile;
+    }
+
+    // Method for reading in our Personnel_Data_File
+    public static ArrayList<String> readPersDataFile() throws Exception {
+        ArrayList<String> personnelFile = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader("Personnel_Data_File.txt"));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            personnelFile.add(line);
+        }
+        reader.close();
+        return personnelFile;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        // Create two ArrayLists to hold our data files
+        ArrayList<String> payDataFile = new ArrayList<String>();
+        ArrayList<String> persDataFile = new ArrayList<String>();
+        payDataFile = readPayDataFile();
+        persDataFile = readPersDataFile();
+        PayRoll firstPayroll = new PayRoll(payDataFile.get(0), payDataFile.get(1), payDataFile.get(2), payDataFile.get(3));
+        Personnel firstPersonnel = new Personnel(persDataFile.get(0), persDataFile.get(1), persDataFile.get(2), persDataFile.get(3), persDataFile.get(4));
+        System.out.println(firstPersonnel.employeeNo);
+        System.out.println(firstPersonnel.lastNameFirstName);
+        System.out.println(firstPersonnel.streetAddress);
+        System.out.println(firstPersonnel.cityStateZip);
+        System.out.println(firstPersonnel.hourlyRate);
+    }
+}
