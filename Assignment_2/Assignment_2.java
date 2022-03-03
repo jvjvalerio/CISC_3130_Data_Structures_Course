7b075914323e32518213eca8d62f77e1b592be3e
Julio Valerio <julio.valerio2@live.lagcc.cuny.edu>
2022-03-03 14:43:54 -0500

    Modify PayRoll Constructors Reformat PayRoll and Personnel Data files

--- Assignment_2/Assignment_2.java
+++ Assignment_2/Assignment_2.java
@@ -6,41 +6,32 @@ import java.util.ArrayList;
 class PayRoll {
     String employeeNo = "";
     String numOfDays = "";
-    String firstStartTime = "";
-    String firstEndTime = "";
-    String secondStartTime = "";
-    String secondEndTime = "";
-    String thirdStartTime = "";
-    String thirdEndTime = "";
+    String firstDay = "";
+    String secondDay = "";
+    String thirdDay = "";
 
     // PayRoll Constructor for only one day
-    public PayRoll(String employeeNo, String numOfDays, String firstStartTime, String firstEndTime) {
+    public PayRoll(String employeeNo, String numOfDays, String firstDay) {
         this.employeeNo = employeeNo;
         this.numOfDays = numOfDays;
-        this.firstStartTime = firstStartTime;
-        this.firstEndTime = firstEndTime;
+        this.firstDay = firstDay;
     }
 
     // PayRoll Constructor overloaded for two days
-    public PayRoll(String employeeNo, String numOfDays, String firstStartTime, String firstEndTime, String secondStartTime, String secondEndTime) {
+    public PayRoll(String employeeNo, String numOfDays, String firstDay, String secondDay) {
         this.employeeNo = employeeNo;
         this.numOfDays = numOfDays;
-        this.firstStartTime = firstStartTime;
-        this.firstEndTime = firstEndTime;
-        this.secondStartTime = secondStartTime;
-        this.secondEndTime = secondEndTime;
+        this.firstDay = firstDay;
+        this.secondDay = secondDay;
     }
 
     // PayRoll Constructor overloaded for three days
-    public PayRoll(String employeeNo, String numOfDays, String firstStartTime, String firstEndTime, String secondStartTime, String secondEndTime, String thirdStartTime, String thirdEndTime) {
+    public PayRoll(String employeeNo, String numOfDays, String firstDay, String secondDay, String thirdDay) {
         this.employeeNo = employeeNo;
         this.numOfDays = numOfDays;
-        this.firstStartTime = firstStartTime;
-        this.firstEndTime = firstEndTime;
-        this.secondStartTime = secondStartTime;
-        this.secondEndTime = secondEndTime;
-        this.thirdStartTime = thirdStartTime;
-        this.thirdEndTime = thirdEndTime;
+        this.firstDay = firstDay;
+        this.secondDay = secondDay;
+        this.thirdDay = thirdDay;
     }
 }
 
@@ -68,7 +59,7 @@ public class Assignment_2 {
     // Method for reading in our Payroll_Data_File
     public static ArrayList<String> readPayDataFile() throws Exception {
         ArrayList<String> paymentFile = new ArrayList<String>();
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/untitled/Desktop/CISC_3130_Data_Structures_Course/Assignment_2/Payroll_Data_File.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("Payroll_Data_File.txt"));
         String line = null;
         while ((line = reader.readLine()) != null) {
             paymentFile.add(line);
@@ -80,7 +71,7 @@ public class Assignment_2 {
     // Method for reading in our Personnel_Data_File
     public static ArrayList<String> readPersDataFile() throws Exception {
         ArrayList<String> personnelFile = new ArrayList<String>();
-        BufferedReader reader = new BufferedReader(new FileReader("/Users/untitled/Desktop/CISC_3130_Data_Structures_Course/Assignment_2/Personnel_Data_File.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("Personnel_Data_File.txt"));
         String line = null;
         while ((line = reader.readLine()) != null) {
             personnelFile.add(line);
@@ -94,32 +85,44 @@ public class Assignment_2 {
         // Create two ArrayLists to hold our data files
         ArrayList<String> payDataFile = new ArrayList<String>();
         ArrayList<String> persDataFile = new ArrayList<String>();
-        ArrayList<Personnel> arrayOfPersonnel = new ArrayList<Personnel>();
         ArrayList<PayRoll> arrayOfPayroll = new ArrayList<PayRoll>();
+        ArrayList<Personnel> arrayOfPersonnel = new ArrayList<Personnel>();
         
         payDataFile = readPayDataFile();
         persDataFile = readPersDataFile();
 
-        int h = 0;
-        for (int i = 0; i < 6; i++) {
-            if (payDataFile.get(i) == "1") {
-                arrayOfPayroll.add(new PayRoll(payDataFile.get(h-1), payDataFile.get(h), payDataFile.get(h+1), payDataFile.get(h+2)));
-            } else if (payDataFile.get(i) == "2") {
-                arrayOfPayroll.add(new PayRoll(payDataFile.get(h-1), payDataFile.get(h), payDataFile.get(h+1), payDataFile.get(h+2), payDataFile.get(h+3), payDataFile.get(h+4)));
-            } else if (payDataFile.get(i) == "3") {
-                arrayOfPayroll.add(new PayRoll(payDataFile.get(h-1), payDataFile.get(h), firstStartTime, firstEndTime, secondStartTime, secondEndTime, thirdStartTime, thirdEndTime))
+        // For loop to populate arrayOfPayRoll ArrayList with objects
+        for (int i = 0; i < payDataFile.size(); i++) {
+            if (payDataFile.get(i).equals("1")) {
+                arrayOfPayroll.add(new PayRoll(payDataFile.get(i-1), payDataFile.get(i), payDataFile.get(i+1)));
+            } else if (payDataFile.get(i).equals("2")) {
+                arrayOfPayroll.add(new PayRoll(payDataFile.get(i-1), payDataFile.get(i), payDataFile.get(i+1), payDataFile.get(i+2)));
+            } else if (payDataFile.get(i).equals("3")) {
+                arrayOfPayroll.add(new PayRoll(payDataFile.get(i-1), payDataFile.get(i), payDataFile.get(i+1), payDataFile.get(i+2), payDataFile.get(i+3)));
             }
-            h++;
         }
         
+        // For loop to populate arrayOfPersonnel ArrayList with objects
         int j = 0;
         for (int k = 0; k < 6; k++) {
             arrayOfPersonnel.add(new Personnel(persDataFile.get(j), persDataFile.get(j+1), persDataFile.get(j+2), persDataFile.get(j+3), persDataFile.get(j+4), persDataFile.get(j+5)));
             j+=6;
         }
 
+        // Test print loop for checking ArrayList of Personnel Objects
         for (Personnel obj : arrayOfPersonnel) {
-            System.out.print(obj.employeeNo + " " + obj.lastNameFirstName + " " + obj.streetAddress + " " + obj.cityStateZip + " " + obj.hourlyRate + " " + obj.blankSpace+ "\n");
+            System.out.print(obj.employeeNo + "\n" + obj.lastNameFirstName + "\n" + obj.streetAddress + "\n" + obj.cityStateZip + "\n" + obj.hourlyRate + "\n" + obj.blankSpace+ "\n");
+        }
+
+        // Test print loop for checking ArrayList of PayRoll Objects
+        for (PayRoll obj : arrayOfPayroll) {
+            if (obj.numOfDays.equals("1")) {
+                System.out.println(obj.employeeNo + "\n" + obj.numOfDays + "\n" + obj.firstDay + "\n");    
+            } else if (obj.numOfDays.equals("2")) {
+                System.out.println(obj.employeeNo + "\n" + obj.numOfDays + "\n" + obj.firstDay + "\n" + obj.secondDay + "\n");    
+            } else if (obj.numOfDays.equals("3")) {
+                System.out.println(obj.employeeNo + "\n" + obj.numOfDays + "\n" + obj.firstDay + "\n" + obj.secondDay + "\n" + obj.thirdDay + "\n");
+            }
         }
     }
 }
